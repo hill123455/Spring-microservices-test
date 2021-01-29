79473c064e863a521d79bc5865a7f15f2482dc93
hiu <ws.fortminor@gmail.com>
2021-01-29 15:35:45 +0700

    Update config server

---
+++ config-server/src/main/java/hiu/com/ConfigurationServerApp.java
@@ -0,0 +1,14 @@
+package hiu.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigurationServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServerApp.class, args);
+	}
+}
